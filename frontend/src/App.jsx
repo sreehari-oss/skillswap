3dfce0d678ba8a1f2c8f72de0dc7f566689ffd57
sreehari-oss <sreeharih036@gmail.com>
2026-09-19 05:54:37 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://skillswap-backend-fte2.onrender.com";
 
 function App() {
   const [gigs, setGigs] = useState([]);
